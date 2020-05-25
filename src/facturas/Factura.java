c9c21955edc661f3d021b4f2731aa7e42a8d96f6
LorenzoB <lorenzoB@gmail.com>
2020-05-25 16:59:58 +0200

    Implementando la interfaz Ifractura.java a la clase fractura.java, sin terminar de implementar los metodos

--- src/facturas/Factura.java
+++ src/facturas/Factura.java
@@ -1,6 +1,6 @@
 package facturas;
 
-public class Factura {
+public  class Factura implements Ifactura{
 	
 	public int num;
 	public float base;
@@ -10,6 +10,25 @@ public class Factura {
 	public static void main (String[] args) {
 		
 	}
+
+	@Override
+	public void cambiarEstado(Estados estado) {
+		
+	}
+
+	@Override
+	public float getTotal() {
+		return 0;
+	}
+
+	@Override
+	public float getCantidadIva() {
+		return 0;
+	}
+
+	
+	
+	
 
 	
 }
