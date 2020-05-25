441b9dc134af577da2b7514c9dafc65871017b5a
LorenzoA <lorenzoA@gmail.com>
2020-05-25 16:32:34 +0200

    Corrección de un bug: Cambiamos la visibilidad el atributo estado de public a private, en la clase factura.java

--- src/facturas/Factura.java
+++ src/facturas/Factura.java
@@ -5,7 +5,7 @@ public class Factura {
 	public int num;
 	public float base;
 	public float tipoIva;
-	public Estados estado;
+	private Estados estado;
 	
 	public static void main (String[] args) {
 		
