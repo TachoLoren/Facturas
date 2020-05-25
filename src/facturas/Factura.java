1ec6582f5b0db58f6f63c6d43947391c0397346b
LorenzoB <lorenzoB@gmail.com>
2020-05-25 16:07:31 +0200

    Creación de la clase factura.java

---
+++ src/facturas/Factura.java
@@ -0,0 +1,14 @@
+package facturas;
+
+public class Factura {
+	
+	public int num;
+	public float base;
+	public float tipoIva;
+	public Estados estado;
+	
+	public static void main (String[] args) {
+		
+	}
+
+}
